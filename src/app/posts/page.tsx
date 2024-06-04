b3bd003968edda0602bf00ab559f26c9f33564ad
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-06-04 07:48:40 +0530

    initial base layout created

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <main className="text-center pt-16 px-5">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">All posts</h1>
+    </main>
+  );
+}
+
+export default page;
