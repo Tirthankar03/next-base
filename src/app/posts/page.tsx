38603370e085f56c6b0c6287d6193d9a9213c899
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-06-04 09:44:45 +0530

    data fetching in server component done

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,9 +1,25 @@
+import Link from "next/link";
 import React from "react";
 
-function page() {
+async function page() {
+  //server component
+  //fetch data
+  //normally fetch() is not used, you directly access your db
+  //no need to use useEffect like react and vite app
+  const res = await fetch("https://dummyjson.com/posts?limit=10");
+  const data = await res.json();
+
   return (
     <main className="text-center pt-16 px-5">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">All posts</h1>
+
+      <ul>
+        {data.posts.map((post) => (
+          <li key={post.id} className="mb-3">
+            <Link href={`/posts/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
