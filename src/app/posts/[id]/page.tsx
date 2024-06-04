84bdf6fad541d18d4ebe147ad0f472bd71371cd3
Tirthankar03 <tirthankar.nath2@gmail.com>
2024-06-04 10:00:19 +0530

    dynamic page implemented using the app router

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+async function page({params}: {id: string} ) {
+    const res = await fetch(`https://dummyjson.com/posts/${params.id}`);
+    const post = await res.json();
+  return (
+    <main className=' px-7 pt-24 text-pretty text-center'>
+        <h1 className='text-5xl font-semibold mb-7'>{post.title}</h1>
+        <p className=' max-w-[700px] mx-auto'>{post.body}</p>
+    </main>
+  )
+}
+
+export default page
